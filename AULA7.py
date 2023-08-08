559ec88e5913cb0847ade28118bde677443eed5c
EmersonBSilv <emerson_bsilva1@hotmail.com>
2023-08-07 22:31:08 -0300

    Aula sobre dicionario.py + github

--- AULA7.py
+++ AULA7.py
@@ -1,1 +1,17 @@
 # AULA 7 + GITHUB
+
+carros = {"Jeep Renegade":['R$90.000,00', '2018'],
+            "Jeep Compass": ['R$ 150.000,00', '2020'],
+            "Troller": ['R$ 200.000,00', '2021']}
+
+print(carros)
+carros ["Jeep Compass"] = 'R$ 180.000,00'
+
+del carros["Troller"]
+print (carros)
+
+carros ["Audi"] = ['R$ 250.000,00, "2023']
+print(carros)
+
+print ("Audi" in carros)
+print ('BMW' in carros)
